b21f72d49b8937e058a597518b59ebe71a021cb2
adithyasunil26 <adithyasunil26@gmail.com>
2021-02-12 17:24:41 +0400

    added wires for fetch and decode

--- y86-64.v
+++ y86-64.v
@@ -5,9 +5,31 @@
 `include "Write_back/write_back.v"
 
 module y86();
+
+  wire [31:0] pc;
+
+  wire [3:0] fetch_icode; 
+	wire [3:0] fetch_ifun; 
+	wire [3:0] fetch_rA; 
+	wire [3:0] fetch_rB;
+	wire [31:0] fetch_valC; 
+	wire [31:0] fetch_valP; 
+
+  wire [3:0] decode_icode; 
+	wire [3:0] decode_ifun; 
+	wire [3:0] decode_rA; 
+	wire [3:0] decode_rB; 
+	wire [31:0] decode_valA; 
+	wire [31:0] decode_valB; 
+	wire [31:0] decode_valC; 
+	wire [31:0] decode_valP; 
+
+
+//instantiating each module
   fetch f();
   decode d();
   execute e();
   memory m();
   write_back wb();
+
 endmodule
