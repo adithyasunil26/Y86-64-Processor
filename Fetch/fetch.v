3fc103f81f4284271e4407e7916cfee744a9c46a
adithyasunil26 <adithyasunil26@gmail.com>
2021-02-23 03:23:27 +0400

    fetch

--- Fetch/fetch.v
+++ Fetch/fetch.v
@@ -1,19 +1,38 @@
 module fetch(
-  clk,
-  //instruction fields
+  clk,  pc, mem,
   icode, ifun, rA, rB, valC, valp,
-  reg1, reg2, val1, val2,
-  icodeo, ifuno, rAo, rBo, valA, valB, valCo,valPo 
-
 );
 
-  input clk;
+  input reg clk;
+  input reg [63:0] pc;
+  input reg [7:0] mem [0:1024];
 
-  input reg [3:0] icode;
-  input reg [3:0] ifun;
-  input reg [3:0] rA;
-  input reg [3:0] rB;
-  input reg [3:0] valC;
-  input reg [3:0] valP;
+  output reg [3:0] icode;
+  output reg [3:0] ifun;
+  output reg [3:0] rA;
+  output reg [3:0] rB;
+  output reg [3:0] valC;
+  output reg [3:0] valP;
+
+  reg [63:0] handled;
+
+  always @ (posedge clk) begin
+    handled<= 0;
+    if(handled<size) begin
+      case(handled)
+        0:begin
+          read<=1;
+          address <=pc/2;
+          size<= (icode == 0 || icode == 1 || icode == 9) ? 1 :
+                    ((icode == 2 || icode == 6 || icode == 'hA || icode == 'hB) ? 2 :
+                    ((icode ==  7 || icode == 8) ? 9 : 10));
+        end
+        1:begin
+
+        end  
+      endcase
+    end
+    valP <= pc;
+  end
 
 endmodule
