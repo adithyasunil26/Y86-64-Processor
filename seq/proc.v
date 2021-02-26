190380e7fa527d5a28d19fcf6035230c21a352df
adithyasunil26 <adithyasunil26@gmail.com>
2021-02-27 03:25:24 +0400

    proc

--- seq/proc.v
+++ seq/proc.v
@@ -1,20 +1,21 @@
 `timescale 1ns / 1ps
 
-module proc()
+module proc();
   
   reg clk;
 
   wire [63:0] pc;
   pipeline=64'bz;
-  
+
   wire [3:0] fetch_icode; 
 	wire [3:0] fetch_ifun; 
 	wire [3:0] fetch_rA; 
 	wire [3:0] fetch_rB;
-	wire [31:0] fetch_valC; 
-	wire [31:0] fetch_valP; 
+	wire [63:0] fetch_valC;
 
-  fetch();
+	wire [63:0] fetch_valP; 
+
+  fetch(PC,fetch_icode,fetch_ifun,fetch_rA,fetch_rB,fetch_valC);
   decode();
   execute();
   memory();
