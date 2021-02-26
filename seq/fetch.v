9b6be65603ee6578a0309decef5d7dea2b46ea60
adithyasunil26 <adithyasunil26@gmail.com>
2021-02-27 03:00:31 +0400

    fetch

--- seq/fetch.v
+++ seq/fetch.v
@@ -175,15 +175,50 @@ module fetch(
   icode= instr[0:3];
   ifun= instr[4:7];
 
+  if(icode==4'b0010) //cmovxx
+  begin
+    rA=instr[8:11];
+    rB=instr[12:15];
+  end
+  if(icode==4'b0011) //irmovq
+  begin
+    rA=instr[8:11];
+    rB=instr[12:15];
+  end
+  if(icode==4'b0100) //rmmovq
+  begin
+    rA=instr[8:11];
+    rB=instr[12:15];
+    valC=instr[16:79];
+  end
+  if(icode==4'b0101) //mrmovq
+  begin
+    rA=instr[8:11];
+    rB=instr[12:15];
+    valC=instr[16:79];
+  end
   if(icode==4'b0110) //OPq
   begin
     rA=instr[8:11];
     rB=instr[12:15];
-    valP=PC+2;
   end
-  if(icode==4'b0110)
+  if(icode==4'b0111) //jxx
   begin
-
+    valC=instr[8:71]
+  end
+  if(icode==4'b1000) //call
+  begin
+    valC=instr[8:71]
+  end
+  if(icode==4'b1010) //pushq
+  begin
+    rA=instr[8:11];
+    rB=instr[12:15];
+  end
+  if(icode==4'b1011) //popq
+  begin
+    rA=instr[8:11];
+    rB=instr[12:15];
   end
   // always@(posedge clk)
   // begin 
