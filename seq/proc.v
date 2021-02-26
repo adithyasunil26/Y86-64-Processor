fa55c99017f29b3b31e346e4964a1c2e60e29582
adithyasunil26 <adithyasunil26@gmail.com>
2021-02-26 04:14:01 +0400

    started

---
+++ seq/proc.v
@@ -0,0 +1,15 @@
+module proc()
+  
+  wire [63:0] pc;
+
+  wire [3:0] fetch_icode; 
+	wire [3:0] fetch_ifun; 
+	wire [3:0] fetch_rA; 
+	wire [3:0] fetch_rB;
+	wire [31:0] fetch_valC; 
+	wire [31:0] fetch_valP; 
+
+  
+
+
+endmodule
