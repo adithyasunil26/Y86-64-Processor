9e9ce9a46a3403011f24353ccd2a7e0fb4fd0863
adithyasunil26 <adithyasunil26@gmail.com>
2021-02-27 02:36:25 +0400

    decode

--- seq/decode.v
+++ seq/decode.v
@@ -1,9 +1,29 @@
 `timescale 1ns / 1ps
 
 module decode(
-
+  
 )
 
-
+  if(icode==4'b0110)
+  begin
+    rA=instr[8:11];
+    rB=instr[12:15];
+    if(ifun==4'b0000)
+    begin
+      
+    end
+    if(ifun==4'b0001)
+    begin
+    
+    end
+    if(ifun==4'b0010)
+    begin
+    
+    end
+    if(ifun==4'b0011)
+    begin
+    
+    end
+  end
 
 endmodule
