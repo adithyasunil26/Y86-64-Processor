f11e2190271e4d7e229b6c20b4eb475d072e2bea
adithyasunil26 <adithyasunil26@gmail.com>
2021-03-01 19:43:03 +0400

    exec

--- seq/execute.v
+++ seq/execute.v
@@ -14,60 +14,63 @@ module execute(
   output reg [63:0] valE; 
   output reg CC;
 
-  if(icode==4'b0010) //cmovxx
+  always@(icode)
   begin
-    valE=64'd0+valA;
-  end
-  if(icode==4'b0011) //irmovq
-  begin
-    valE=64'd0+valC;
-  end
-  if(icode==4'b0100) //rmmovq
-  begin
-    valE=valB+valC;
-  end
-  if(icode==4'b0101) //mrmovq
-  begin
-    valE=valB+valC;
-  end
-  if(icode==4'b0110) //OPq
-  begin
-    if(ifun==4'b0000) //add
+    if(icode==4'b0010) //cmovxx
     begin
-      //valE=valA+valB;
+      valE=64'd0+valA;
     end
-    if(ifun==4'b0001) //sub
+    if(icode==4'b0011) //irmovq
     begin
-      //valE=valA-valB;
+      valE=64'd0+valC;
     end
-    if(ifun==4'b0010) //and
+    if(icode==4'b0100) //rmmovq
     begin
-      //valE=valA.valB;
+      valE=valB+valC;
     end
-    if(ifun==4'b0011) //xor
+    if(icode==4'b0101) //mrmovq
     begin
-      //valE=valA^valB;
+      valE=valB+valC;
     end
-  end
-  if(icode==4'b0111) //jxx
-  begin
-    //cnd=cond(cc,ifun)
-  end
-  if(icode==4'b1000) //call
-  begin
-    valE=-64'd8+valB;
-  end
-  if(icode==4'b1001) //ret
-  begin
-    valE=64'd8+valB;
-  end
-  if(icode==4'b1010) //pushq
-  begin
-    valE=-64'd8+valB;
-  end
-  if(icode==4'b1011) //popq
-  begin
-    valE=64'd8+valB;
+    if(icode==4'b0110) //OPq
+    begin
+      if(ifun==4'b0000) //add
+      begin
+        //valE=valA+valB;
+      end
+      if(ifun==4'b0001) //sub
+      begin
+        //valE=valA-valB;
+      end
+      if(ifun==4'b0010) //and
+      begin
+        //valE=valA.valB;
+      end
+      if(ifun==4'b0011) //xor
+      begin
+        //valE=valA^valB;
+      end
+    end
+    if(icode==4'b0111) //jxx
+    begin
+      //cnd=cond(cc,ifun)
+    end
+    if(icode==4'b1000) //call
+    begin
+      valE=-64'd8+valB;
+    end
+    if(icode==4'b1001) //ret
+    begin
+      valE=64'd8+valB;
+    end
+    if(icode==4'b1010) //pushq
+    begin
+      valE=-64'd8+valB;
+    end
+    if(icode==4'b1011) //popq
+    begin
+      valE=64'd8+valB;
+    end
   end
 
 endmodule
