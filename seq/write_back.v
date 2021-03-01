99aa392e33ddb45a5116ea2b8216da28162001c1
adithyasunil26 <adithyasunil26@gmail.com>
2021-03-01 19:45:26 +0400

    wb

--- seq/write_back.v
+++ seq/write_back.v
@@ -12,46 +12,48 @@ module write_back(
   input reg [63:0] valE;
   input reg [63:0] valM;
 
+  always@(icode)
+  begin
+    if(icode==4'b0010) //cmovxx
+    begin
+      reg_mem[rB]=valE;
+    end
+    if(icode==4'b0011) //irmovq
+    begin
+      reg_mem[rB]=valE;
+    end
+    // if(icode==4'b0100) //rmmovq
+    // begin
+    // end
+    if(icode==4'b0101) //mrmovq
+    begin
+      reg_mem[rA]=valM;
+    end
+    if(icode==4'b0110) //OPq
+    begin
+      reg_mem[rB]=valE;
+    end
+    // if(icode==4'b0111) //jxx
+    // begin
+    // end
+    if(icode==4'b1000) //call
+    begin
+      reg_mem[4]=valE;
+    end
+    if(icode==4'b1001) //ret
+    begin
+      reg_mem[4]=valE;
+    end
 
-  if(icode==4'b0010) //cmovxx
-  begin
-    reg_mem[rB]=valE;
-  end
-  if(icode==4'b0011) //irmovq
-  begin
-    reg_mem[rB]=valE;
-  end
-  // if(icode==4'b0100) //rmmovq
-  // begin
-  // end
-  if(icode==4'b0101) //mrmovq
-  begin
-    reg_mem[rA]=valM;
-  end
-  if(icode==4'b0110) //OPq
-  begin
-    reg_mem[rB]=valE;
-  end
-  // if(icode==4'b0111) //jxx
-  // begin
-  // end
-  if(icode==4'b1000) //call
-  begin
-    reg_mem[4]=valE;
-  end
-  if(icode==4'b1001) //ret
-  begin
-    reg_mem[4]=valE;
-  end
-
-  if(icode==4'b1010) //pushq
-  begin
-    reg_mem[4]=valE;
-  end
-  if(icode==4'b1011) //popq
-  begin
-    reg_mem[4]=valE;
-    reg_mem[rA]=valM;
+    if(icode==4'b1010) //pushq
+    begin
+      reg_mem[4]=valE;
+    end
+    if(icode==4'b1011) //popq
+    begin
+      reg_mem[4]=valE;
+      reg_mem[rA]=valM;
+    end
   end
 
 endmodule
