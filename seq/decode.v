4fb2ba2f43bcd0b45cebd0565474ed583066f670
adithyasunil26 <adithyasunil26@gmail.com>
2021-03-01 19:42:23 +0400

    decode

--- seq/decode.v
+++ seq/decode.v
@@ -12,48 +12,51 @@ module decode(
   output reg [63:0] valA;
   output reg [63:0] valB; 
 
-  if(icode==4'b0010) //cmovxx
+  always@(icode)
   begin
-    valA=reg_mem[rA];
-  end
-  // if(icode==4'b0011) //irmovq
-  // begin
-  // end
-  if(icode==4'b0100) //rmmovq
-  begin
-    valA=reg_mem[rA];
-    valB=reg_mem[rB];
-  end
-  if(icode==4'b0101) //mrmovq
-  begin
-    valB=reg_mem[rB];
-  end
-  if(icode==4'b0110) //OPq
-  begin
-    valA=reg_mem[rA];
-    valB=reg_mem[rB];
-  end
-  // if(icode==4'b0111) //jxx
-  // begin
-  // end
-  if(icode==4'b1000) //call
-  begin
-    valB=reg_mem[4]; //rsp
-  end
-  if(icode==4'b1001) //ret
-  begin
-    valA=reg_mem[4]; //rsp
-    valB=reg_mem[4]; //rsp
-  end
-  if(icode==4'b1010) //pushq
-  begin
-    valA=reg_mem[rA];
-    valB=reg_mem[4]; //rsp
-  end
-  if(icode==4'b1011) //popq
-  begin
-    rA=reg_mem[4]; //rsp
-    rB=reg_mem[4]; //rsp
+    if(icode==4'b0010) //cmovxx
+    begin
+      valA=reg_mem[rA];
+    end
+    // if(icode==4'b0011) //irmovq
+    // begin
+    // end
+    if(icode==4'b0100) //rmmovq
+    begin
+      valA=reg_mem[rA];
+      valB=reg_mem[rB];
+    end
+    if(icode==4'b0101) //mrmovq
+    begin
+      valB=reg_mem[rB];
+    end
+    if(icode==4'b0110) //OPq
+    begin
+      valA=reg_mem[rA];
+      valB=reg_mem[rB];
+    end
+    // if(icode==4'b0111) //jxx
+    // begin
+    // end
+    if(icode==4'b1000) //call
+    begin
+      valB=reg_mem[4]; //rsp
+    end
+    if(icode==4'b1001) //ret
+    begin
+      valA=reg_mem[4]; //rsp
+      valB=reg_mem[4]; //rsp
+    end
+    if(icode==4'b1010) //pushq
+    begin
+      valA=reg_mem[rA];
+      valB=reg_mem[4]; //rsp
+    end
+    if(icode==4'b1011) //popq
+    begin
+      rA=reg_mem[4]; //rsp
+      rB=reg_mem[4]; //rsp
+    end
   end
 
 endmodule
