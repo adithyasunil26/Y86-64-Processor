3100eb0e6db1ea5a603491bad034d6407629a4f9
adithyasunil26 <adithyasunil26@gmail.com>
2021-03-09 03:38:23 +0400

    ereg

--- pipe-/e_reg.v
+++ pipe-/e_reg.v
@@ -2,20 +2,26 @@
 
 module e_reg(
   clk,
-  d_stat,d_icode,d_ifun,d_valC,d_valA,d_valB,
-  e_stat,e_icode,e_ifun,e_valC,e_valA,e_valB
+  d_stat,d_icode,d_ifun,d_rA,d_rB,d_valC,d_valP,d_valA,d_valB,
+  e_stat,e_icode,e_ifun,e_rA,e_rB,e_valC,e_valP,e_valA,e_valB
 );  
   input clk;
   input [2:0]  d_stat;
   input [3:0]  d_icode;
   input [3:0]  d_ifun;
+  input [3:0]  d_rA;
+  input [3:0]  d_rB;
   input [63:0] d_valC;
+  input [63:0] d_valP;
   input [63:0] d_valA;
   input [63:0] d_valB;
   output reg [2:0]  e_stat;
   output reg [3:0]  e_icode;
   output reg [3:0]  e_ifun;
+  output reg [3:0]  e_rA;
+  output reg [3:0]  e_rB;
   output reg [63:0] e_valC;
+  output reg [63:0] e_valP;
   output reg [63:0] e_valA;
   output reg [63:0] e_valB;
 
