51a395f7e96960d9234548c2b5eacaae8142aa67
adithyasunil26 <adithyasunil26@gmail.com>
2021-03-09 03:39:55 +0400

    wreg

--- pipe-/w_reg.v
+++ pipe-/w_reg.v
@@ -6,20 +6,43 @@ module w_reg(
   w_stat,w_icode,w_valE,w_valM
 );  
   input clk;
-  input [2:0]  m_stat ;
+  
+  input [2:0]  m_stat;
   input [3:0]  m_icode;
-  input [63:0] m_valE ;
-  input [63:0] m_valM ;
-  output reg [2:0]   w_stat ;
-  output reg [3:0]   w_icode;
-  output reg [63:0]  w_valE ;
-  output reg [63:0]  w_valM ;
+  input [3:0]  m_rA;
+  input [3:0]  m_rB;
+  input [63:0] m_valC;
+  input [63:0] m_valP;
+  input [63:0] m_valA;
+  input [63:0] m_valB;
+  input        m_cnd;
+  input [63:0] m_valE;
+  input [63:0] m_valM;
+
+  output reg [2:0]  w_stat;
+  output reg [3:0]  w_icode;
+  output reg [3:0]  w_rA;
+  output reg [3:0]  w_rB;
+  output reg [63:0] w_valC;
+  output reg [63:0] w_valP;
+  output reg [63:0] w_valA;
+  output reg [63:0] w_valB;
+  output reg        w_cnd;
+  output reg [63:0] w_valE;
+  output reg [63:0] w_valM;
 
   always@(posedge clk)
   begin
-    w_stat = m_stat ;
-    w_icode= m_icode;
-    w_valE = m_valE ;
-    w_valM = m_valM ;
+    w_stat    =   m_stat;
+    w_icode   =   m_icode;
+    w_rA      =   m_cnd;
+    w_rB      =   m_rA;
+    w_valC    =   m_rB;
+    w_valP    =   m_valC;
+    w_valA    =   m_valP;
+    w_valB    =   m_valA;
+    w_cnd     =   m_valB;
+    w_valE    =   m_valE;
+    w_valM    =   m_valM;
   end
 endmodule
